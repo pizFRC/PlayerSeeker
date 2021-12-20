97b643190af9e51226fea59ccc6d200bf28eeee8
pizFRC <dfzfnc00m29c352t@studenti.unical.it>
2021-12-20 21:38:01 +0100

    PSE-4 #close #time 20m  #comment Progetto creato

---
+++ src/main/java/mat/unical/it/PlayerSeeker/PlayerSeeker/PlayerSeekerApplication.java
@@ -0,0 +1,13 @@
+package mat.unical.it.PlayerSeeker.PlayerSeeker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerSeekerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerSeekerApplication.class, args);
+	}
+
+}
