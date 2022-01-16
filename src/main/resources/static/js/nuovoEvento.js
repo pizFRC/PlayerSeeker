22dc8a68ed4df8e6c16dde34281a660165ee31be
pizFRC <dfzfnc00m29c352t@studenti.unical.it>
2022-01-16 01:59:52 +0100

    modificata pagina di creazione evento

--- src/main/resources/static/js/nuovoEvento.js
+++ src/main/resources/static/js/nuovoEvento.js
@@ -46,38 +46,189 @@ const geojson = {
 }
 ]
 };
+
+
        
    function next(){
 	
 	
-	
-	//	console.log($(".dn").first().filter(".d-n"));
-	
-$(".d-n:first").removeClass('d-n').addClass('d-y') ;
+	//valida dati
+	//validateData();
 
-if($(".d-n:first").attr("id")=="third_step"){
-addAddressInput();
-	}
-console.log("4");
+	//modifica la progress bar
+	
+	/* if($(".d-n:first").attr("id")=="third_step"){
+	   
+     
+	   }*/
+	updateProgress(true);
+      $(".d-y:first").next().addClass('d-y');
+       $(".d-y:first").next().removeClass('d-n');
+  $(".d-y:first").addClass('d-n').removeClass('d-y');
+   
+  $("span:not(.border-dark)").first().addClass("border-dark");
+
+     //) $(".d-y:last").prev().addClass('d-n');//
+                     //      .removeClass('d-y');
+
+            //   $(".d-y:first").addClass('d-n')
+console.log(  $(".d-n:first"))
+console.log(   $(".d-n:last"))
+
+
+console.log(  $(".d-n:first").next())
+console.log(   $(".d-n:last").next())
+
+console.log(  $(".d-n:first").prev())
+console.log(   $(".d-n:last").prev())
+
+
+
+
+
+     
+console.log(  $(".d-y:first"))
+console.log(   $(".d-y:last"))
+
+console.log(  $(".d-y:first").next())
+console.log(   $(".d-y:last").next())
+
+     
+console.log(  $(".d-y:first").prev)
+console.log(   $(".d-y:last").prev)
+          //il primo elemento con classe d-n ,e lo rendo visibile
+      /*$(".d-y:first").addClass('d-n');
+        $(".d-n:first").removeClass('d-y') ;
+          //  $(".d-y:first").removeClass('d-n');
+      console.log( $(".d-y:first") );
+
+
+       /*  $(".d-y:first").addClass('d-n');
+           $(".d-n:first").removeClass('d-y');
+    //$(".d-y:first").addClass('d-n').removeClass('d-y') ;
+      console.log( $(".d-y:first") );//.removeClass('d-y').addClass('d-n') ;
+          //Controllo se il div con il time picker  è già presente
+     */
+	
 }
 
 
    function prev(){
-	
-	      $(".d-y:last").removeClass('d-y').addClass('d-n');
+	if($(".d-y:first").attr('id')=="first_step")
+	    return;
+		updateProgress(false);
+		$(".d-y:last").prev().addClass('d-y');
+	      $(".d-y:first").removeClass('d-n');
+$(".d-y:last").addClass('d-n').removeClass('d-y');
+
+
+$("span.border-dark:last").removeClass("border-dark");
+/*   $(".d-y:first").next().addClass('d-y');
+       $(".d-y:first").next().removeClass('d-n');
+  $(".d-y:first").addClass('d-n').removeClass('d-y');
+         // $(".d-y:first").removeClass('d-y').addClass('d-n');
+        */  
 }
 
 
-	$(document).ready(function() {
-	
-     
-      var sports;
+
+
+///////////////TIME PICKER////////////
+
+function creaTimePicker(){
+	const popup = new dhx.Popup({
+    css: "dhx_widget--bordered"
+});
+
+const timepicker = new dhx.Timepicker();
+const result = document.getElementById("result");
+timepicker.events.on("change", function (res) {
+    result.value = res;
+});
+
+popup.attach(timepicker);
+
+document.querySelector("#show").addEventListener("click", function () {
+    popup.show("show");
+});
+}
+
+///////////////TIME PICKER CONFIG////////////
+
+$(document).ready(function(){
+   
+
+
+
+
+$('#ora_inizio').timepicker({
+    timeFormat: 'HH:mm ',
+    interval: 60,
+    minTime: '08:00',
+    maxTime: '23:00',
+    defaultTime: '08',
+    startTime: '08:00',
+    dynamic: false,
+    dropdown: true,
+    scrollbar: true
+});
+
+$('#ora_fine').timepicker({
+   timeFormat: 'HH:mm ',
+    interval: 60,
+    minTime: '09:00',
+    maxTime: '23:00',
+    defaultTime: '09',
+    startTime: '09:00',
+    dynamic: false,
+    dropdown: true,
+    scrollbar: true
+});
+
+ 
+ 
+
+
+});
+///////////////PROGRESS BAR UPDATE FUNCTION////////////
 
 	
-	
+  function updateProgress(add){
+	//	<div class="progress-bar bg-dark" role="progressbar" style="width: 25%;"
+				//	aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+				
+				let el = document.getElementById('progress_bar');
+		if(add)	{
+		    var	aria_val_now=(parseInt(el.getAttribute("aria-valuenow"))+25);
+			var width=(parseInt(el.style.width  )+25);	
+				}else{
+					if((parseInt(el.style.width  )<=0))
+					return;
+		    var	aria_val_now=(parseInt(el.getAttribute("aria-valuenow"))-25);
+			var width=(parseInt(el.style.width  )-25);	
+				
+				}
+				el.setAttribute("aria-valuenow",aria_val_now);
+				
+				el.style.width =width+"%";
+		
+}
+
+
+
+
+
+
+///////////////CARICA I TIPI DI SPORT E LI AGGIUNGE AL CAROUSEL////////////
+
+	$(document).ready(function() {
+		 $('#privacy').prop('checked', false);
+	   addAddressInput();
+     
+    var sports;
 
 	sportsContainer = document.querySelector(".carousel");
-
+     
 	
 	$.ajax({
 		type: "POST",
@@ -112,24 +263,22 @@ console.log("4");
 				sportsContainer.append(div);
 			
 				$(document).on('click','#'+sport.type,function(e){
-					document.getElementById("num_giocatori").value=sport.requiredPlayers; 
-			if(($("#second_step").hasClass("d-y")))
-			return;		
-			
-			next();
-					//$("#set_giocatori").children().remove();
-					
-				});
 				
+				//modifico il numero di giocatori richiesti
+				document.getElementById("num_giocatori").value=sport.requiredPlayers; 
+				
+				//devo anche rivedere le strutture che praticano quello sport
+			
+			   //Se clicco su uno sport type del carousel non vado avanti ma ritorno alla scelta della fascia oraria
+			       if(($("#second_step").hasClass("d-y")))
+			             return;		
+		              
+                 next();
+	
+				       });
 			
 				});
 		
-				//<label class="btn btn-outline-secondary" for="btn-check-2-outlined">Checked</label>
-				/*
-				
-			*/
-
-			
     	},
 	 	statusCode: {
     		503: function() {
@@ -137,9 +286,10 @@ console.log("4");
  				 }
 		}
 	});
+	//aggiungo al carousel 
 	 document.querySelector("#first_step").append(sportsContainer);
 
-		
+///////////////CONFIGURO IL CAROUSEL//////////////////
 $('.carousel').slick({
   dots: true,
   infinite: false,
@@ -170,68 +320,79 @@ $('.carousel').slick({
         slidesToScroll: 1
       }
     }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
+   
   ]
 });
+$('#privacy').click(function() {
+  if ($(this).is(':checked')) {
+   $( "#confirm_btn" ).prop( "disabled", false );
+$( "#confirm_btn" ).removeClass('btn-light').addClass('btn-success');
+  }else{
+	$( "#confirm_btn" ).prop( "disabled", true );
+	$( "#confirm_btn" ).removeClass('btn-success').addClass('btn-light');
 
-
-	});
 	
-	//FUNZIONI RELATIVE ALLA MAPPA
+}
+})
+	
+	
+	});
+
+	///////////////AGGIUNGO LA MAPPA///////////
 function addAddressInput(){
 		
 		mapboxgl.accessToken = 'pk.eyJ1IjoiZ3ZuYmVyYWxkaSIsImEiOiJja3kwMTY1cjQydXVtMnZvMHI3N3B6Y2piIn0.BVrI0Ru6h55mmhivqa-39Q';
-						const map = new mapboxgl.Map({
-							container : 'map',
-							style : 'mapbox://styles/mapbox/light-v10',
-						
-							center : [ 16.2537 ,39.2983],
-							zoom : 8,
-					
-						});
-						
-						
-						
-map.on('idle',function(){
-		map.resize()
-
-})
-     map.on('load',function(){
+		const map = new mapboxgl.Map({
+		            container : 'map',
+		            style : 'mapbox://styles/mapbox/light-v10',
+			     	center : [ 16.2537 ,39.2983],
+				    zoom : 8,
+                    });
 	
-map.resize()
-})
+              map.on('idle',function(){
+		          
+                 map.resize()
 
-map.on('render', () => {
-			
-map.resize()
-console.log('A render event occurred.');
-});
+                })
+              map.on('load',function(){
+	
+                 map.resize()
+                })
+
+               map.on('render', () => {
+			         
+                    map.resize() 
+                });
 
 
 			 map.addControl(
-new MapboxGeocoder({
-		 placeholder: 'Cerca qui la tua città',
-					 types:'place,region',			
-		accessToken: mapboxgl.accessToken,
-		mapboxgl: mapboxgl
-	}).on('result', function(e) {
-	alert(e.result.geometry);
-		InserisciMarkerVicini(mapboxgl,map);
+                 new MapboxGeocoder({
+	   			       placeholder: 'Cerca qui la tua città',
+					  types:'place,region',			
+				      accessToken: mapboxgl.accessToken,
+					 mapboxgl: mapboxgl
+				}).on('result', function(e) {
+				         alert(e.result.geometry);
+
+                     //devo anche passare la città(lat,lang)del giocatore e una lista di strutture
+				        InserisciMarkerVicini(mapboxgl,map);
 		
-	}).on('results',function(e){
-		rimuoviMarker();
-	})
-);			
-
-}
+				}).on('results',function(e){
+					    rimuoviMarker();
+				})
+			);			
+ console.log("MAPPA CRE");
+       }
 	
-function rimuoviMarker(){
-	$( ".marker" ).remove();
+	
+	
+     function rimuoviMarker(){
+	     $( ".marker" ).remove();
 
-}
+      }
 
+
+//MODIFICARE QUESTA FUNZIONE ,CREARE UNA FUNZIONE PER CREARE IL DIV DEL MARKER
 function InserisciMarkerVicini(mapboxgl,map){
  
 // Add markers to the map.
@@ -275,7 +436,7 @@ function creaInput(placeHolder){
    return input;
 }
 
-
+///////////////FUNZIONI PER AGGIUNGERE INPUT NUOVO GIOCATORE ->NOME  COGNOME  ///////////
 function addInputNameGiocatore(){
 if(document.getElementById("num_giocatori").value==0)
 { 
@@ -292,7 +453,7 @@ divContenitore.className="input-group mt-2";
 var btnMeno=document.createElement("button"); 
 btnMeno.className="btnbtn-outline-secondary";
 var id=document.getElementById("set_giocatori").children.length;
-console.log("creo btnMeno_"+ id);
+
 btnMeno.id="btnMeno_"+id; 
 divContenitore.id="div_"+id;
 var spanMeno=document.createElement("span");
@@ -308,27 +469,17 @@ spanMeno.append(iconMeno);
 	e.preventDefault();
 	e.stopPropagation();
 	
-console.log( "id button "+$(this).attr('id'));
-
- 
 
 //document.getElementById("btnMeno_"+id).removeEventListener("click",this);
 if(!document.getElementById("btnMeno_"+id))
 return;
+
+
 document.getElementById("btnMeno_"+id).parentElement.remove();
 
-
-
-            
        
 document.getElementById("num_giocatori").value=parseInt(document.getElementById("num_giocatori").value)+1;
          
-              
-       
-
-
-
-
 
 });
 
@@ -339,23 +490,44 @@ document.getElementById("set_giocatori").append(divContenitore);
 
            
 }
+
+
+		$(document).ready(function() {
+	
 ////////////////////////////
 
 
-/*
-map.on('load', () => {
+function validatePlayerForm(){
+	console.log("player valid");
+   		var validator = $('#second_step').validate();
 
-  map.addLayer({
-    id: 'strutture',
-    type: 'symbol',
-    source: {
-      type: 'geojson',
-      data: strutture
-    },
-    layout: {
-      'icon-image': 'url(../img/gym.png)',
-        
-    },
-    paint: {}
-  });
-});*/
+	$("#data_input").rules("add", {
+  		required: true,
+  		dateISO: true,
+  		messages: {
+    		required: "Inserisci il tuo nome",
+			
+  		}
+	});
+
+	//$("#data_input").valid();
+	//$("#data_input").valid();
+	$("#prova").rules("add", {
+  		required: true,
+  		maxlength: 20,
+        minlength:3,
+  		messages: {
+    		required: "Inserisci il tuo cognome",
+			maxlength: "il cognome deve essere lungo al massimo 20 caratteri",
+			minlength: "L'username deve essere lungo almeno 5 caratteri"
+  		}
+	});
+
+	
+	
+}
+});
+
+
+
+
--- src/main/webapp/WEB-INF/jsp/nuovoEvento.jsp
+++ src/main/webapp/WEB-INF/jsp/nuovoEvento.jsp
@@ -5,7 +5,7 @@
 <html lang="it">
 <head>
 
-
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.css">
 
 	<!-- Slick CSS-->
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css"/>
@@ -30,7 +30,8 @@
 }
 </style>
 
-
+	
+		
 <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.7.2/mapbox-gl-geocoder.min.js"></script>
 <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.7.2/mapbox-gl-geocoder.css" type="text/css">
 
@@ -56,142 +57,198 @@
  	<jsp:include page="header.jsp" />
 	
 	<!-- FORM -->
-<div class="container  shadow-lg bg-body bg-dark rounded p-5" id="form_creazione_evento">
+<div class="container shadow rounded-2 bg-body bg-dark p-5" id="form_creazione_evento">
 
-		<div class=" needs-validation container w-75 mx-auto row" novalidate>
-            <div class="col-md-12">
+		<form class=" needs-validation container-fluid w-75 mx-auto " id="form_evento">
+		
+	
+          <div class="row">
+
+
+            <div class="col-12" id="title">
             
             <h1 class="h1">Seleziona uno sport per l'evento da creare</h1>
             
             </div>
-            <div id="first_step" >
-            <div class="carousel d-flex m-4 mt-3" >
-              
-            
-            </div>
-			</div>
+
+			<div  id="contenitore" class="col-12 mb-3"  style="position: relative; height: 20px;">
 			
-				<div class="col-md-12 mb-3 d-n " id="second_step">
-				<div class="row">
-					<div class="col">
-					<label class="form-label" for="data">Data</label>
-						<input type="date" class="form-control"  id="data"	name="date">
-					</div>
-					<div class="col">
-					<label class="form-label" for="time">Ora</label>
-						<input type="time" class="form-control" id="time" name="time">
-					</div>
+			<div id="step" class="d-flex justify-content-between w-100 "  style="z-index:9;">
+			
+				    <span class="badge badge-pill border border-2 border-dark point bg-light "  ><i class="fas fa-volleyball-ball" ></i></span>
+			<span class="badge badge-pill point border border-2 bg-light  " ><i class="far fa-calendar-alt"></i> </span>
+				<span class="badge badge-pill point border border-2 bg-light  " ><i class="fas fa-map-marked-alt"></i>  </span>
+			<span class="badge badge-pill point border border-2 bg-light " > <i class="fas fa-users-cog"></i> </span>
+			<span class="badge badge-pill  point border border-2 bg-light  " >  <i class="fas fa-check-circle"></i>  </span> 	
+			</div>
+				
+				<div  id="progress" class="progress " style="height: 1px;z-index:6;">
+				
+					<div id="progress_bar" class="progress-bar bg-dark "
+						role="progressbar" style="width: 0%;" aria-valuenow="0"
+						aria-valuemin="0" aria-valuemax="100">
+						
 					</div>
 				</div>
+				
+				
+			</div>
 			
-			<div class="col-md-12 mb-5 d-n"   id="third_step"> 
-			 
-			<h1> Seleziona una struttura</h1>
-			
-			
-			
-			
+             <!-- Carousel -->
+			<div  class="col-12 d-y" id="first_step">
+				<div class="carousel d-flex m-4 mt-3"></div>
+			</div>
+
+            <!-- TIME PICKER -->
+				<div class="col-12 mb-3 d-n " id="second_step" name="second_step">
+
+					<div class="row">
+
+						<div class="col-sm-10">
+							<label for="data_input" class="form-label">Data</label> 
+							<input type="date" class="form-control" id="data_input"
+								name="data_input" placeholder="" value="" required="">
+							<div class="invalid-feedback">Valid first name is required.
+							</div>
+						</div>
+
+						<div class="col-sm-6">
+							<label for="ora_inizio" class="form-label">Ora inizio</label> 
+							<input   class="timepicker form-control" id="ora_inizio"
+								name="ora_inizio" required="" >
+							<div class="invalid-feedback">Valid first name is required.
+							</div>
+						</div>
+
+						<div class="col-sm-6">
+							<label for="ora_fine" class="form-label">Ora Fine</label> <input
+								 class="timepicker form-control" id="ora_fine"
+								name="ora_fine" required="">
+							<div class="invalid-feedback">Valid first name is required.
+							</div>
+						</div>
+
+
+
+
+
+					</div>
+
+				</div>
+
+
+				<!-- MAP -->
+			<div class="col-12 mb-5 d-n" id="third_step">
+
+				<h1>Seleziona una struttura</h1>
+
+
+
+
+
+				<div class="container border border-2 rounded" id="map"></div>
+
+
+
+
+			</div>
+
 		
-          	<div class="container  border border-2 rounded"   id="map">
-			</div>
 
-				
-				
-               
-                </div>
+           <!-- SET NUMERO GIOCATORI -->
+			<div class="col-12 mb-3 d-n" id="four_step">
 
-          	<div class="col-md-12 mb-3 d-n " id="upload_file">
-					
-						<label class="form-label" for="uploadImage">Upload</label>
-						<select>
-						<option></option>
-						
-						
-						
-						</select>
-					
-				</div>
-
-
-			<div class="col-md-12 mb-3  d-n"  id="four_step">
-			
-				<label class="form-label" for="num_giocatori">Numero giocatori mancati</label>
+				<label class="form-label" for="num_giocatori">Numero
+					giocatori mancati</label>
 				<div class="input-group mb-3">
 					<input type="text" class="form-control" id="num_giocatori"
 						placeholder="" aria-label="" aria-describedby="button-addon2"
 						disabled pattern="[0-9]+">
-					<button class="btn btn-outline-secondary" type="button" onclick="addInputNameGiocatore()" id="button-addon2">
-						<span><i class="fa fa-plus" aria-hidden="true"></i></span> 
-						<span class="sr-only">Previous</span>
+					<button class="btn btn-outline-secondary" type="button"
+						onclick="addInputNameGiocatore()" id="button-addon2">
+						<span><i class="fa fa-plus" aria-hidden="true"></i></span> <span
+							class="sr-only">Previous</span>
 					</button>
 
 				</div>
 
-				<div id="set_giocatori" class="container-fluid">
-				</div>
-			 </div>
-
-			<div class="col-md-12 mb-3 d-n " id="upload_file">
-					
-						<label class="form-label" for="uploadImage">Upload</label>
-						<input type="file" class="form-control" id="uploadImage">
-					
-				</div>
-			
-
-
-				
-			
-			
-				<div class="container mt-4  d-n "  id="comment_step">
-					
-				
-				
-						<div class="container mb-3 mt-3">
-							<label for="comment">Inserisci un promemoria per l'evento:</label>
-							<textarea class="form-control w-75" rows="5" id="comment" name="text"></textarea>
-						</div>
-						
-					
-				</div>
-				
-				
-				<div class="form-group col-md-12  d-n "  id="last_step">
-				<div class="form-check">
-					<input class="form-check-input" type="checkbox" value=""
-						id="invalidCheck" required> <label
-						class="form-check-label" for="invalidCheck"> Agree to
-						terms and conditions </label>
-					<div class="invalid-feedback">You must agree before
-						submitting.</div>
-				</div>
-				<button class="btn btn-primary" type="submit">Submit form</button>
+				<div id="set_giocatori" class="container-fluid"></div>
 			</div>
-				
+
+
+
+   <!-- COMMENTO
+			<div class="col-12 mt-4  d-n " id="comment_step">
+
+				<div class="container mb-3 mt-3">
+					<label for="comment">Inserisci un promemoria per l'evento:</label>
+					<textarea class="form-control w-100" rows="5" id="comment"
+						name="text"></textarea>
+				</div>
+
 			</div>
-		
+			 -->
+			
+
+
+
+
+
+
+
+
+               <!-- CONDIZIONI PRIVACY CHECKBOX AND CONFERMA CREAZIONE BUTTON-->
+               
+               
+               
+               <div class="form-check col-12 d-n " id="last_step">
+               <div>
+            <input type="checkbox" class="form-check-input" id="privacy">
+            <label class="form-check-label" for="same-address" onclick="prova()">Shipping address is the same as my billing address</label>
+            </div>
+           <div class="d-flex ">
+				 <button class="btn btn-light mx-auto" disabled   id="confirm_btn" type="submit">Fine </button>
+				
+				</div>
+          </div>
           
-      <div class="col-md-12">
-            
-            <div class="row">
-            
-            <div class="col-md-6">
-            <button class="btn btn-outline-secondary float-start " type="button" onclick="prev()" id="btn_prev">
-						<span><i class="fa fa-angle-left" aria-hidden="true"></i></span> 
-						<span class="sr-only">Previous</span>
+            <!-- TASTI AVANTI E INDIETRO -->
+          <div class="col-md-12">
+
+			<div class="d-flex justify-content-around">
+
+				<div  >
+					<button class="btn btn-outline-secondary float-start "
+						type="button" onclick="prev()" id="btn_prev">
+						<span><i class="fa fa-angle-left" aria-hidden="true"></i></span> <span
+							class="sr-only">Previous</span>
 					</button>
 				</div>
-				 <div class="col-md-6 ">	
-					<button class="btn btn-outline-secondary float-end" type="button" onclick="next()" id="btn_next">
-						<span><i class="fa fa-angle-right" aria-hidden="true"></i></span> 
+				
+				
+				<div >
+					<button class="btn btn-outline-secondary float-end" type="button"
+						onclick="next()" id="btn_next">
+						<span><i class="fa fa-angle-right" aria-hidden="true"></i></span>
 						<span class="sr-only">Next</span>
 					</button>
-					</div>
-            
-            </div>
-	</div>
-	
-	
+				</div>
+
+			</div>
+
+
+		</div>
+			
+			<!-- END ROW -->
+			
+          </div>
+		</form>
+
+
+     
+		
+
+
 	</div>
 	
 	<!-- end FORM -->
@@ -212,7 +269,8 @@
 	<!-- JQuery -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.3/dist/jquery.validate.js"></script>
-	
+	<script src="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.js"></script>
+
 	
 		
 	<!--Slick -->
